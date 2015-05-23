2bcad1f41f1baa53079a9f2be5e40fca4b78ba70
Andrias Meisyal <andriasonline@gmail.com>
2015-05-23 21:03:27 +0700

    add rgb to grayscale converter functionality

--- RPCServer.rb
+++ RPCServer.rb
@@ -1,10 +1,28 @@
 require 'jimson'
+require 'base64'
+require 'mini_magick'
 
 class MyHandler
   extend Jimson::Handler
 
-  def convert(data)
-    receive = data
+  def convert(data, filename)
+    # edit temporary path as you want
+    temppath = 'your/temporary/path/here'
+    # decode received data from base64
+    File.open(temppath + filename, 'wb') do |file|
+      file.write(Base64.decode64(data))
+    end
+    # convert to grayscape using ImageMagick and RMagick
+    image = MiniMagick::Image.open(temppath + filename)
+    image = image.colorspace("Gray")
+    image.write(temppath + filename)
+
+    # encode converted image
+    f = File.open(temppath + filename, "rb")
+    contents = Base64.encode64(f.read)
+    f.close
+
+    receive = contents
   end
 end
 
